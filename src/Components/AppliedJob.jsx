e51180621bd1d2bb286dd8bb495a9a798da4cb90
safin33221 <saifn33221@gmail.com>
2024-11-02 16:31:38 +0600

    completed apply job section

--- src/Components/AppliedJob.jsx
+++ src/Components/AppliedJob.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import { getApplyedJobFromLS } from "./Utilits/addToDb";
+import ApplyJob from "./ApplyJob";
 
 
 const AppliedJob = () => {
+    const storedJob = getApplyedJobFromLS()
+    const storedJobInt = storedJob.map(id => parseInt(id))
+    const [jobData,setJobData] = useState([])
+    useEffect(()=>{
+        fetch('/jobs.json')
+        .then(res => res.json())
+        .then(data =>setJobData(data))
+    },[])
+
+
+    const applyedJobs = jobData.filter(job => storedJobInt.includes(job.id))
+   
     return (
         <div>
-            <h1>this is applyed job section</h1>
+            {
+                applyedJobs.map(job => <ApplyJob key={job.id} job={job}></ApplyJob>)
+            }
         </div>
     );
 };
---
+++ src/Components/ApplyJob.jsx
@@ -0,0 +1,33 @@
+
+
+const ApplyJob = ({job}) => {
+    const {id, logo, job_title, company_name, remote_or_onsite, location, salary } = job
+    return (
+        <div>
+            <div className="flex gap-10 border-2 shadow-lg my-5 py-6 rounded-lg ">
+                <div className="flex justify-center items-center p-3 py-10 m-3 bg-gray-200 rounded-lg ">
+                    <img className="" src={logo} alt="" />
+                </div>
+                <div className="flex w-full items-center justify-between space-y-3">
+                    <div>
+                        <h1 className="text-xl font-bold">{job_title}</h1>
+                        <p className="text-gray-500">{company_name}</p>
+                        <div className="flex gap-4">
+                            <button className="btn btn-outline btn-sm btn-primary">{remote_or_onsite}</button>
+                            <button className="btn btn-outline btn-sm btn-primary">Full Time</button>
+                        </div>
+                        <div className="flex gap-4 items-center">
+                            <p><span className="text-lg font-bold">Location:</span> {location}</p>
+                            <p><span className="text-lg font-bold">Salary:</span> {salary}</p>
+                        </div>
+                    </div>
+                    <div className="px-10">
+                        <button className="btn btn-primary ">Viwe Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ApplyJob;
