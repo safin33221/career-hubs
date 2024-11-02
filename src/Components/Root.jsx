e4cfbdc7b34823735c3c529fb03126e9ef2814b8
safin33221 <saifn33221@gmail.com>
2024-11-02 10:48:20 +0600

    initalize setup done

---
+++ src/Components/Root.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
