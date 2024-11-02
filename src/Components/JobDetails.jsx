81042a44202d116847b100d12e8a62da28c45f6c
safin33221 <saifn33221@gmail.com>
2024-11-02 15:42:37 +0600

    add apply now btn in job details section

--- src/Components/JobDetails.jsx
+++ src/Components/JobDetails.jsx
@@ -6,7 +6,7 @@ const JobDetails = () => {
     const JobId = parseInt(id)
     const jobData = useLoaderData()
     const job = jobData.find(job => job.id === JobId)
-    const { company_name,job_description, salary, job_title, contact_information, job_responsibility, educational_requirements, experiences } = job
+    const { company_name, job_description, salary, job_title, contact_information, job_responsibility, educational_requirements, experiences } = job
 
     return (
         <div>
@@ -20,18 +20,23 @@ const JobDetails = () => {
                     <h1 className='text-gray-600'><span className='text-black font-bold text-lg'> Educational Requirements: <br /> </span>{educational_requirements}</h1>
                     <h1 className='text-gray-600'><span className='text-black font-bold text-lg'>experiences: <br /></span>{experiences}</h1>
                 </div>
-                <div className='w-1/3 shadow-lg p-4  '>
-                    <h1 className='text-xl font-bold'>{company_name}</h1>
-                    <div className='border-t-2 my-6'></div>
-                    <h1 className='text-black font-bold'>Salary: <span className='text-gray-600'>{salary}</span></h1>
-                    <h1 className='text-black font-bold'>Job Title:<span className='text-gray-600'>{job_title}</span></h1>
-                    <div className='border-t-2 my-6'></div>
+                <div className='w-1/3' >
+                    <div className=' shadow-lg p-4  '>
+                        <h1 className='text-xl font-bold'>{company_name}</h1>
+                        <div className='border-t-2 my-6'></div>
+                        <h1 className='text-black font-bold'>Salary: <span className='text-gray-600'>{salary}</span></h1>
+                        <h1 className='text-black font-bold'>Job Title:<span className='text-gray-600'>{job_title}</span></h1>
+                        <div className='border-t-2 my-6'></div>
 
-                    <h1 className='text-lg font-bold'>Contact Information</h1>
-                    <div className='border-t-2 my-2'></div>
-                    <h1><span className='text-black font-bold'>Phone:</span> {contact_information.phone}</h1>
-                    <h1><span className='text-black font-bold'>Email:</span> {contact_information.email}</h1>
-                    <h1><span className='text-black font-bold'>Address:</span> {contact_information.address}</h1>
+                        <h1 className='text-lg font-bold'>Contact Information</h1>
+                        <div className='border-t-2 my-2'></div>
+                        <h1><span className='text-black font-bold'>Phone:</span> {contact_information.phone}</h1>
+                        <h1><span className='text-black font-bold'>Email:</span> {contact_information.email}</h1>
+                        <h1><span className='text-black font-bold'>Address:</span> {contact_information.address}</h1>
+                    </div>
+                    <div className='py-5'>
+                        <button className='btn btn-primary flex w-full'> Apply Now</button>
+                    </div>
 
 
                 </div>
